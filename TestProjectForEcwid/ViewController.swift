e288be788fdbc844a6f106f637a9ec56f70afebc
IsaikinSergei <akinfest007@gmail.com>
2021-02-15 01:06:19 +0400

    Initial Commit

---
+++ TestProjectForEcwid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProjectForEcwid
+//
+//  Created by Sergei Isaikin on 15.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
