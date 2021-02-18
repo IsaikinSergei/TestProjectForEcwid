2fc59115ab8771e0a073ef551ea2e2aa5c98b0dc
IsaikinSergei <akinfest007@gmail.com>
2021-02-18 21:28:22 +0400

    Added screenshots

--- TestProjectForEcwid/ViewControllers/CustomTableViewCell.swift
+++ TestProjectForEcwid/ViewControllers/CustomTableViewCell.swift
@@ -8,11 +8,9 @@
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var imageClothes: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var quantityLabel: UILabel!
-    
-
 }
